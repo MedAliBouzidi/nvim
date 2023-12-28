51ea5512f8182f2e2707d014f840d4796e005d98
BOUZIDI Mohamed Ali <bouzidi.medali20@gmail.com>
2023-12-28 03:03:45 +0100

    add lsp-config & fix Neo-tree issues

---
+++ lua/plugins/lsp-congig.lua
@@ -0,0 +1,51 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require('mason-lspconfig').setup({
+        ensure_installed = {
+          "clangd", "jdtls", "pyright",
+          "eslint", "emmet_ls", "html", "cssls", "tailwindcss",
+          "tsserver", "jsonls", "sqlls",
+          "dockerls", "docker_compose_language_service",
+          "lua_ls", "bashls", "yamlls"
+        }
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require('lspconfig')
+      local opts = {}
+
+      lspconfig.clangd.setup({})
+      lspconfig.jdtls.setup({})
+      lspconfig.pyright.setup({})
+      lspconfig.eslint.setup({})
+      lspconfig.emmet_ls.setup({})
+      lspconfig.html.setup({})
+      lspconfig.cssls.setup({})
+      lspconfig.tailwindcss.setup({})
+      lspconfig.tsserver.setup({})
+      lspconfig.jsonls.setup({})
+      lspconfig.sqlls.setup({})
+      lspconfig.dockerls.setup({})
+      lspconfig.docker_compose_language_service.setup({})
+      lspconfig.lua_ls.setup({})
+      lspconfig.bashls.setup({})
+      lspconfig.yamlls.setup({})
+
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+      vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+      vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+    end
+  }
+}
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,13 +1,25 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-	},
-	config = function()
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  config = function()
+    vim.keymap.set('n', '<leader>e', ':Neotree filesystem reveal left toggle<CR>')
 
-		vim.keymap.set('n', '<leader>e', ':Neotree filesystem reveal left<CR>')
-	end
+    require("neo-tree").setup({
+      filesystem = {
+        window = {
+          mappings = {
+            -- disable split from neo-tree by mapping to none or noop
+            ["S"] = "noop",
+            ["s"] = "none"
+          }
+        }
+      }
+    })
+
+  end
 }
