2fb486677a8b1324cb4b0405dfc07218a27a27ad
BOUZIDI Mohamed Ali <bouzidi.medali20@gmail.com>
2024-03-28 14:34:31 +0100

    add vim-tmux-navigator

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -8,3 +8,12 @@ vim.cmd("set shiftwidth=2")
 vim.cmd("set number relativenumber")
 
 vim.cmd("autocmd FileType c setlocal noexpandtab tabstop=8 softtabstop=0 shiftwidth=8")
+
+vim.g.background = "light"
+vim.opt.swapfile = false
+
+-- Navigate nvim panes
+vim.keymap.set("n", "<C-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<C-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<C-h>", ":wincmd h<CR>")
+vim.keymap.set("n", "<C-l>", ":wincmd l<CR>")
